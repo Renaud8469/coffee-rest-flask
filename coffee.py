c84a141c06d217616351d1a46b384ced2bad9c3e
Renaud Dahl <renauddahl@gmail.com>
2017-06-13 15:38:02 +0200

    Add order lookup

--- coffee.py
+++ coffee.py
@@ -20,3 +20,22 @@ def place_order():
     resp.status_code = 201
     return resp
 
+
+@app.route('/orders/<int:order_id>', methods=['GET','PUT'])
+def order(order_id):
+    order = orders[order_id]
+    if request.method == 'PUT':
+        # Check if order is modifiable
+        if order['prepared']:
+            code = 409
+        else:
+            body = request.get_json()
+            order["drink"] = body["drink"]
+            code = 200
+    else:
+        code = 200
+    # Return the response
+    resp = Response(json.dumps(order))
+    resp.status_code = code
+    return resp
+
--- tests_coffee.py
+++ tests_coffee.py
@@ -7,12 +7,27 @@ class CoffeeTestCase(unittest.TestCase):
 
     def setUp(self):
         self.app = coffee.app.test_client()
+        self.order = {"drink":"tea", "cost":1.5, "prepared": False}
 
     def test_order_creation(self):
         resp = self.app.post('/orders/', data=json.dumps({"drink":"tea"}), content_type='application/json')
         resp_data = json.loads(resp.data.decode('utf-8'))
-        order = {"drink":"tea", "cost":1.5, "prepared": False}
-        self.assertDictEqual(resp_data, order)
+        self.assertDictEqual(resp_data, self.order)
+        self.assertEqual(resp.headers['Location'], "http://localhost/orders/0")
+        self.assertEqual(resp.status_code, 201)
+
+    def test_order_read(self):
+        resp = self.app.get('/orders/0')
+        resp_data = json.loads(resp.data.decode('utf-8'))
+        self.assertDictEqual(resp_data, self.order)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_order_update(self):
+        resp = self.app.put('/orders/0', data=json.dumps({"drink":"expresso"}), content_type='application/json')
+        resp_data = json.loads(resp.data.decode('utf-8'))
+        self.order["drink"] = "expresso"
+        self.assertDictEqual(resp_data, self.order)
+        self.assertEqual(resp.status_code, 200)
 
 
 if __name__ == '__main__':
